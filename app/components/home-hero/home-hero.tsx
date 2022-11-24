1b85c9c6ec952266f203741ac7eafacdde6a9bc8
Miguel Prada <mapra99@gmail.com>
2022-11-23 23:41:07 -0500

    tablet styles

--- app/components/home-hero/home-hero.tsx
+++ app/components/home-hero/home-hero.tsx
@@ -6,15 +6,15 @@ const HomeHero = ({ product }: HomeHeroProps) => {
 
   return (
     <section className="bg-brown">
-      <div className="box-border min-h-128 py-28 px-6 relative z-0">
+      <div className="box-border min-h-128 py-28 px-6 relative z-0 sm:min-h-160 sm:px-48 sm:py-32">
         <div className="flex flex-col justify-center items-center">
-          <Text variant="overline" className="opacity-50 text-white mb-4" as="span">
+          <Text variant="overline" className="opacity-50 text-white mb-4 sm:mb-6" as="span">
             New Product
           </Text>
-          <Text variant="heading-1" className="text-4.5xl text-white text-center tracking-wide mb-6">
+          <Text variant="heading-1" className="!text-4.5xl text-white text-center tracking-wide mb-6 sm:!text-6xl">
             { name }
           </Text>
-          <Text variant="body" className="text-white opacity-75 text-center mb-7">
+          <Text variant="body" className="text-white opacity-75 text-center mb-7 sm:mb-10">
             { featured_description }
           </Text>
           <ButtonLink variant="primary" to={`products/${slug}`}>
