e80c28bbddc8b324898456b34d1978f010ba895d
Miguel Prada <mapra99@gmail.com>
2022-11-23 22:54:09 -0500

    build product model

---
+++ app/models/product/schema.ts
@@ -0,0 +1,33 @@
+import { z } from 'zod'
+
+export const ProductSchema = z.object({
+  name: z.string(),
+  slug: z.string(),
+  base_price: z.number(),
+  featured: z.boolean(),
+  total_quantity: z.number(),
+  created_at: z.string(),
+  updated_at: z.string(),
+  image: z.object({
+    url: z.string()
+  }),
+  category: z.object({
+      name: z.string(),
+      slug: z.string(),
+      created_at: z.string(),
+      updated_at: z.string(),
+      image: z.object({
+        url: z.string()
+      })
+  }),
+  contents: z.object({
+    featured_description: z.string().optional(),
+    featured_image: z.array(z.string()).optional(),
+    description: z.string(),
+    features: z.string(),
+    box_content: z.string(),
+    preview_images: z.array(z.string())
+  })
+})
+
+export type Product = z.infer<typeof ProductSchema>;
