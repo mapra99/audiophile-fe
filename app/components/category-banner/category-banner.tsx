4354c7c9afce69345abf70b5f6163e38d808cb0a
Miguel Prada <mapra99@gmail.com>
2022-11-27 19:52:18 -0500

    build responsive styles

--- app/components/category-banner/category-banner.tsx
+++ app/components/category-banner/category-banner.tsx
@@ -5,19 +5,19 @@ const CategoryBanner = ({ category }: CategoryBannerProps) => {
   const { name, slug, image } = category
 
   return (
-    <div className="-mt-16">
+    <div className="-mt-16 flex-1 lg:-mt-20">
       <img
           src={image.url}
           alt={`${name} icon`}
-          className="h-32 mx-auto relative top-16"
+          className="h-32 mx-auto relative top-16 lg:h-40 lg:top-20"
       />
 
-      <div className="bg-gray box-border pt-8 px-10 pb-6 rounded-lg">
+      <div className="bg-gray box-border pt-8 px-2 pb-6 rounded-lg">
         <div className="w-40 h-9 bg-black opacity-20 mx-auto rounded-[50%] blur-xl mb-6" />
         <Text variant="body" as="h2" className="uppercase !font-bold text-center mb-3">
           { name }
         </Text>
-        <ButtonLink variant="tertiary" to={`/categories/${slug}`} className="justify-center !py-0">
+        <ButtonLink variant="tertiary" to={`/categories/${slug}`} className="justify-center !py-0 !px-0 mx-auto">
           Shop
         </ButtonLink>
       </div>
