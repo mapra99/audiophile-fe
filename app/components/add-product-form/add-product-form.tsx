2f83fc3359b292a6d8037d08b4b14cfe14cf9e05
Miguel Prada <mapra99@gmail.com>
2022-12-29 16:57:08 -0500

    fix(purchase cart): handle maximum available units (#62)

--- app/components/add-product-form/add-product-form.tsx
+++ app/components/add-product-form/add-product-form.tsx
@@ -1,6 +1,6 @@
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import invariant from 'tiny-invariant'
-import { QuantityInput, Button } from '~/components'
+import { QuantityInput, Button, Text } from '~/components'
 import { PurchaseCartContext } from '~/contexts/purchase-cart-context'
 
 import type { ChangeEvent } from 'react'
@@ -8,6 +8,7 @@ import type { AddProductFormProps } from './types'
 
 const AddProductForm = ({ stock }: AddProductFormProps) => {
   const { cart, createOrUpdateCart, openCartList } = useContext(PurchaseCartContext)
+  const [reachedMaxQuantity, setReachedMaxQuantity] = useState<boolean>(false)
 
   const handleSubmit = async (e: ChangeEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -25,13 +26,24 @@ const AddProductForm = ({ stock }: AddProductFormProps) => {
 
   const existingItem = cart?.items.find(item => item.stock.uuid === stock.uuid)
 
+  const handleChange = (qty: number) => {
+    setReachedMaxQuantity(qty === stock.quantity)
+  }
+
   return (
-    <form className="flex gap-4 items-center" onSubmit={handleSubmit}>
-      <QuantityInput value={existingItem ? existingItem.quantity : 1} name="quantity" min={0}/>
-      <Button variant="primary" className="py-3" type="submit">
-        { existingItem ? "Update Cart" : "Add to Cart" }
-      </Button>
-    </form>
+    <div>
+      <form className="flex gap-4 items-center" onSubmit={handleSubmit}>
+        <QuantityInput value={existingItem ? existingItem.quantity : 1} name="quantity" onChange={handleChange} min={1} max={stock.quantity} />
+        <Button variant="primary" className="py-3" type="submit">
+          { existingItem ? "Update Cart" : "Add to Cart" }
+        </Button>
+      </form>
+      { reachedMaxQuantity && (
+        <Text variant="body" className="!text-danger !leading-none italic mt-4">
+          There are only { stock.quantity } units available
+        </Text>
+      )}
+    </div>
   )
 }
 
--- app/components/purchase-cart-list-item/purchase-cart-list-item.tsx
+++ app/components/purchase-cart-list-item/purchase-cart-list-item.tsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import { Text, QuantityInput } from '~/components'
 import { TrashCan } from '~/icons'
 import { PurchaseCartContext } from '~/contexts/purchase-cart-context'
@@ -9,8 +9,11 @@ import type { PurchaseCartListItemProps } from './types'
 const PurchaseCartListItem = ({ cartItem }: PurchaseCartListItemProps) => {
   const { createOrUpdateCart, removeCartItem } = useContext(PurchaseCartContext)
   const { uuid, stock: { uuid: stockUuid, product, quantity: stockQuantity }, unit_price, quantity } = cartItem
+  const [reachedMaxQuantity, setReachedMaxQuantity] = useState<boolean>(false)
 
   const handleItemChange = async (qty: number) => {
+    setReachedMaxQuantity(quantity === qty && quantity === stockQuantity)
+
     await createOrUpdateCart({
       stock_uuid: stockUuid,
       quantity: qty
@@ -18,35 +21,43 @@ const PurchaseCartListItem = ({ cartItem }: PurchaseCartListItemProps) => {
   }
 
   return (
-    <div key={uuid} className="flex gap-4 items-center">
-      <div className="rounded-lg bg-gray w-16 h-16 flex items-center justify-center flex-none">
-        { product.image && (
-          <img src={product.image.url} alt="" className="block w-9" />
-        ) }
+    <div>
+      <div key={uuid} className="flex gap-4 items-center">
+        <div className="rounded-lg bg-gray w-16 h-16 flex items-center justify-center flex-none">
+          { product.image && (
+            <img src={product.image.url} alt="" className="block w-9" />
+          ) }
+        </div>
+
+        <div className="flex flex-col flex-1 overflow-hidden">
+          <Text variant="body" className="!font-bold text-ellipsis overflow-hidden" as="span">
+            { product.name }
+          </Text>
+          <Text variant="body" className="!font-bold text-black opacity-50 text-ellipsis overflow-hidden" as="span">
+            { formatCurrency(unit_price) }
+          </Text>
+        </div>
+
+        { quantity === 1 && (
+          <button
+            className="bg-gray h-12 w-12 flex items-center justify-center hover:opacity-70 transition-all"
+            onClick={() => removeCartItem(uuid)}
+            type="button"
+          >
+            <TrashCan className="h-4" />
+          </button>
+        )}
+
+        <div className="flex-none">
+          <QuantityInput value={quantity} onChange={handleItemChange} min={1} max={stockQuantity} />
+        </div>
       </div>
 
-      <div className="flex flex-col flex-1 overflow-hidden">
-        <Text variant="body" className="!font-bold text-ellipsis overflow-hidden" as="span">
-          { product.name }
+      { reachedMaxQuantity && (
+        <Text variant="body" className="!text-danger !leading-none text-right italic">
+          There are only { stockQuantity } units available
         </Text>
-        <Text variant="body" className="!font-bold text-black opacity-50 text-ellipsis overflow-hidden" as="span">
-          { formatCurrency(unit_price) }
-        </Text>
-      </div>
-
-      { quantity === 1 && (
-        <button
-          className="bg-gray h-12 w-12 flex items-center justify-center hover:opacity-70 transition-all"
-          onClick={() => removeCartItem(uuid)}
-          type="button"
-        >
-          <TrashCan className="h-4" />
-        </button>
       )}
-
-      <div className="flex-none">
-        <QuantityInput value={quantity} onChange={handleItemChange} min={1} max={stockQuantity} />
-      </div>
     </div>
   )
 }
